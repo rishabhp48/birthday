398f458ac210753590183ecb4c2b330c25ff6901
Rishabh Pandey <rishabhp48@gmail.com>
2021-02-25 04:11:04 +0530

    Update data.js

--- src/data.js
+++ src/data.js
@@ -8,7 +8,7 @@ export default[
     {
         id:2,
         name:'Bilal Ansari',
-        age:22,
+        age:23,
         image:'https://www.biography.com/.image/ar_8:10%2Cc_fill%2Ccs_srgb%2Cfl_progressive%2Cg_faces:center%2Cq_auto:good%2Cw_620/MTIwNjA4NjM0MjIyODM5MzA4/the-cinema-society--calvin-klein-collection-host-a-screening-of-the-hunger-games---inside-arrivals.jpg'
     },
     {
